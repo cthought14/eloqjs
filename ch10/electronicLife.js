80b80def5230e98bd839d2d5a55b9b7f6d36f018
cthought14 <chaotic_thought@yahoo.com>
2018-01-21 11:10:27 +0200

    Begin Chapter 10 Exercise 2 (electronicLife).

---
+++ ch10/electronicLife.js
@@ -0,0 +1,67 @@
+// electronicLife. Chapter 10, Exercise 2. A return to electronic life.
+"use strict";
+
+// area2d
+// --------------------
+// Vector
+// Grid
+// directions
+// directionNames
+// dirPlus
+
+var area2d = function() {
+    return {
+        Vector: {},
+        Grid: {},
+        directions: {},
+        directionNames: {},
+        dirPlus: {},
+    };
+}();
+      
+
+// util
+// ----------------------
+// randomElement
+
+var util = function() {
+    return {
+        randomElement: {},
+    };
+}();
+
+
+// life
+// ----------------------
+// BouncingCritter
+// World
+// elementFromChar
+// charFromElement
+// Wall
+// View
+// WallFollower
+// LifelikeWorld
+// Plant
+// PlantEater
+// SmartPlantEater
+// Predator
+
+var life = function() {
+    return {
+        BouncingCritter: {},
+        World: {},
+        elementFromChar: {},
+        charFromElement: {},
+        Wall: {},
+        View: {},
+        WallFollower: {},
+        LifelikeWorld: {},
+        Plant: {},
+        PlantEater: {},
+        SmartPlantEater: {},
+        Predator: {},
+    };
+}();
+
+
+console.log("TODO");
