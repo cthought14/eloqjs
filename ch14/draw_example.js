3809e1c4eefd19904b0e5b106c4625984db17afb
cthought14 <chaotic_thought@yahoo.com>
2018-02-05 21:32:34 +0200

    Change touchstart handler.

--- ch14/draw_example.js
+++ ch14/draw_example.js
@@ -14,12 +14,6 @@ drawCanvas.addEventListener("mousemove", function(ev) {
     drawDot(this, ev.pageX - 4, ev.pageY - 4);
 });
 
-drawCanvas.addEventListener("touchstart", function(ev) {
-    if (!penDown)
-        return;
-    drawDot(this, ev.pageX - 4, ev.pageY - 4);
-});
-
 function drawDot(node, x, y) {
     if (ink == 0)
         return;
@@ -40,6 +34,11 @@ function showInk() {
 }
 
 drawCanvas.addEventListener("mousedown", function(ev) {
+    penDown = true;
+    drawDot(this, ev.pageX - 4, ev.pageY - 4);
+});
+
+drawCanvas.addEventListener("touchstart", function(ev) {
     penDown = true;
     drawDot(this, ev.pageX - 4, ev.pageY - 4);
 });
