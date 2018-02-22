de7e3701fc4207c4836c9158d31a4a325d1b360c
cthought14 <chaotic_thought@yahoo.com>
2018-02-22 22:03:22 +0200

    Continue Chapter 16. Next: 314 Curves.

--- ch16/ch16_notes.js
+++ ch16/ch16_notes.js
@@ -37,7 +37,43 @@ function showBorder(canvas) {
 // 312
 //
 tprint("### 312 Filling and stroking");
+var canvas2 = document.getElementById("canvas2");
+var cx2 = canvas2.getContext("2d");
+showBorder(canvas2);
+cx2.strokeStyle = "blue";
+cx2.strokeRect(5, 5, 50, 50);
+cx2.lineWidth = 5;
+cx2.strokeRect(135, 5, 50, 50);
 
+//
+// 313
+//
+tprint("### 313 Paths");
+var canvas3 = document.getElementById("canvas3");
+showBorder(canvas3);
+var cx3 = canvas3.getContext("2d");
+cx3.beginPath();
+for (var y = 10; y < 100; y += 10) {
+    cx3.moveTo(10, y);
+    cx3.lineTo(90, y);
+}
+cx3.stroke();
+
+var canvas4 = document.getElementById("canvas4");
+showBorder(canvas4);
+var cx4 = canvas4.getContext("2d");
+cx4.beginPath();
+cx4.moveTo(50, 10);
+cx4.lineTo(10, 70);
+cx4.lineTo(90, 70);
+// Automatically close the shape by implicitly drawing a line 
+// to (50, 10) before filling.
+cx4.fill(); 
+
+//
+// 314
+//
+tprint("### 314 Curves");
 
 }); /* $(document).ready */
 
