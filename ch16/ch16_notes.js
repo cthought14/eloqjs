3f58bd0db020e025a74c036e8e758f7887b1c9bc
cthought14 <chaotic_thought@yahoo.com>
2018-02-22 07:16:20 +0200

    Begin Chapter 16 (Drawing on Canvas).

---
+++ ch16/ch16_notes.js
@@ -0,0 +1,13 @@
+// Chapter 16 (Drawing on Canvas).
+"use strict";
+
+$(document).ready(function() {
+
+//
+// 297
+//
+tprint("### 297 SVG")
+
+
+}); /* $(document).ready */
+
---
+++ ch16/util.js
@@ -0,0 +1,93 @@
+// Miscellaneous utilities.
+"use strict";
+
+function forEach(collection, fn) {
+    for (var i = 0; i < collection.length; i++) {
+        fn(collection[i], i);
+    }
+}
+
+function clamp(value, min_, max_) {
+    return value < min_ ? min_ : value > max_ ? max_ : value;
+}
+
+// decodeEntities.
+// https://stackoverflow.com/questions/5796718/html-entity-decode
+var decodeEntities = (function() {
+  // this prevents any overhead from creating the object each time
+  var element = document.createElement('div');
+
+  function decodeHTMLEntities (str) {
+    if(str && typeof str === 'string') {
+      // strip script/html tags
+      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+      element.innerHTML = str;
+      str = element.textContent;
+      element.textContent = '';
+    }
+
+    return str;
+  }
+
+  return decodeHTMLEntities;
+})();
+
+// elt(type, child1, child2, ...)
+/*
+function elt(type) {
+    var node = document.createElement(type);
+    for (var i = 1; i < arguments.length; i++) {
+        var child = arguments[i];
+        if (typeof child == "string")
+            child = document.createTextNode(child);
+        node.appendChild(child);
+    }
+    return node;
+}
+*/
+
+// elt(type, className, ...)
+function elt(name, className) {
+    var elt = document.createElement(name);
+    if (className) elt.className = className;
+    for (var i = 2; i < arguments.length; i++) {
+        var child = arguments[i];
+        if (typeof child == "string")
+            child = document.createTextNode(child);
+        elt.appendChild(child);
+    }
+    return elt;
+}
+
+// deepEqual(a : object, b : object) -- True if a and b have the same contents.
+function deepEqual(a, b) {
+    if (a !== null && typeof a == 'object' && 
+        b !== null && typeof b == 'object')
+        // => Need deep comparison.
+    {
+        for (var prop in a) {
+            if (!(prop in b))
+                return false;
+            if (!deepEqual(a[prop], b[prop]))
+                return false;
+        }
+        return true;
+    }
+    return a == b;
+}
+
+////
+////
+
+// in_(prop, obj) -- Similar to (prop in obj).
+function in_(prop, obj) {
+    return obj.hasOwnProperty(prop);
+}
+
+// clearNode(node : Node)
+function clearNode(node) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
