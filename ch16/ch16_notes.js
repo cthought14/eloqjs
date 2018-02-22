be55ce1fd699c5a8ce67c00262467928b5795d13
cthought14 <chaotic_thought@yahoo.com>
2018-02-22 07:33:07 +0200

    Start Chapter 16 notes.

--- ch16/ch16_notes.js
+++ ch16/ch16_notes.js
@@ -4,9 +4,41 @@
 $(document).ready(function() {
 
 //
-// 297
+// 309
 //
-tprint("### 297 SVG")
+tprint("### 309 SVG")
+$("#changeCircle").click(function(ev) {
+    ev.preventDefault();
+    var circle = document.querySelector("circle");
+    if (circle.getAttribute("fill") != "red")
+        circle.setAttribute("fill", "red");
+    else
+        circle.setAttribute("fill", "cyan");
+});
+
+//
+// 310
+//
+tprint("### 310 The canvas element");
+var canvas1 = document.getElementById("canvas1");
+var context1 = canvas1.getContext("2d");
+showBorder(canvas1);
+context1.fillStyle = "red";
+context1.fillRect(10, 10, 100, 50);
+
+function showBorder(canvas) {
+    var w = canvas.width;
+    var h = canvas.height;
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "black";
+    context.rect(0, 0, w, h);
+    context.stroke();
+}
+
+//
+// 312
+//
+tprint("### 312 Filling and stroking");
 
 
 }); /* $(document).ready */
