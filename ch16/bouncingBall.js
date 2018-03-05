152315ca1569a0aa7cf648c5e7edadc5abd580b3
cthought14 <chaotic_thought@yahoo.com>
2018-03-05 07:50:23 +0200

    Clean up code.

--- ch16/bouncingBall.js
+++ ch16/bouncingBall.js
@@ -1,6 +1,9 @@
 // bouncingBall. Chapter 16, Exercise 3. A bouncing ball.
 "use strict";
 
+// From runAnimation.js
+runAnimation
+
 $(document).ready(function() {
 
 //
@@ -11,47 +14,18 @@ var canvas1 = document.getElementById("canvas1");
 showBorder(canvas1);
 var cx1 = canvas1.getContext("2d");
 
-function draw_ball(cx, x, y, s, Cx, Cy) {
-    //cx.save();
-    //cx.translate(x, y);
-    //cx.scale(s, s);
-    
-    // center = (50, 50), radius = 40, angle = 0 to CIRCLE_ANGLE
-    console.log("cx.arc",Cx,Cy);
-    cx.arc(Cx, Cy, 40, 0, CIRCLE_ANGLE);
-    cx.fillStyle = "brown";
-    cx.fill();
-    
-    //cx.restore();
-}
-
-//draw_ball(cx1, 10, 10, 1, 50, 50);
-
 function clear_canvas(canvas, cx) {
-    //var cx = canvas.getContext("2d");
-    //cx.fillStyle = "white";
-    //cx.fillRect(0, 0, 
-    //            canvas.width, canvas.height);
     cx.clearRect(0, 0, 
                  canvas.width, canvas.height);
 }
-/*
-(function(canvas, cx) {
-    $('#move_ball').click(function(ev) {
-        clear_canvas(canvas);
-        showBorder(canvas);
-        draw_ball(cx, 10, 10, 1, 70, 70);
-    });
-})(canvas1, cx1);
-*/
 
-var img33 = document.createElement("img");
-img33.src = "img/ball_80px.png";
-var img33_w = 80;
-var img33_h = 80;
-img33.addEventListener("load", function() {
-    expect("load33", "load33");
-    draw_img(cx1, 0, 0, 1, img33, 20, 20, img33_w, img33_h);
+var ball_img = document.createElement("img");
+ball_img.src = "img/ball_80px.png";
+var ball_w = 80;
+var ball_h = 80;
+ball_img.addEventListener("load", function() {
+    expect("load ball", "load ball");
+    draw_img(cx1, 0, 0, 1, ball_img, 20, 20, ball_w, ball_h);
 });
 
 function draw_img(cx, x, y, s, img, Ix, Iy, Iw, Ih) {
@@ -67,10 +41,6 @@ function draw_img(cx, x, y, s, img, Ix, Iy, Iw, Ih) {
             Ix, Iy, Iw, Ih);
     cx.restore();
 }
-
-// From runAnimation.js
-runAnimation
-
 
 var totalTime = 0;
 var nTimeSamples = 3000;
@@ -108,14 +78,14 @@ $('#move_ball').click(function(ev) {
 
         time("draw_img", function() {
             cx1.save();
-            draw_img(cx1, 0, 0, 1, img33, ballX, ballY, img33_w, img33_h);
+            draw_img(cx1, 0, 0, 1, ball_img, ballX, ballY, ball_w, ball_h);
             cx1.restore();
         });
         
         // hitPole => Would hit north or south of map.
-        var hitPole = ballY < margin || ballY > canvas1.height - img33_h - margin;
+        var hitPole = ballY < margin || ballY > canvas1.height - ball_h - margin;
         // hitMeridian => Would hit east or west of map.
-        var hitMeridian = ballX < margin || ballX > canvas1.width - img33_w - margin;
+        var hitMeridian = ballX < margin || ballX > canvas1.width - ball_w - margin;
         if (hitPole)
             ballDY *= -1;
         if (hitMeridian)
