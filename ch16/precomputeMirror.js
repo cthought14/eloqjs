f18f801217f52ecbc6ff12f247f230cffc731613
cthought14 <chaotic_thought@yahoo.com>
2018-03-06 07:33:11 +0200

    Rename variables.

--- ch16/precomputeMirror.js
+++ ch16/precomputeMirror.js
@@ -16,27 +16,27 @@ tprint("### Exercise 4");
 var canvas1 = document.getElementById("canvas1");
 showBorder(canvas1);
 var cx1 = canvas1.getContext("2d");
-var canvas34_img;
+var canvas_img;
 
 
-var img4 = document.createElement("img");
-img4.src = "img/player.png";
+var img1 = document.createElement("img");
+img1.src = "img/player.png";
 //var spriteW = 24;
 //var spriteH = 30;
-img4.addEventListener("load", function() {
+img1.addEventListener("load", function() {
     expect("load4", "load4");
 
     // HOW TO DRAW SOMETHING ON CANVAS AND THEN MAKE IT INTO AN IMG ELEMENT.    
     // Create temporary canvas. It will hold the flipped image.
     
     var canvas = document.createElement("canvas");
-    canvas.setAttribute("width", img4.width);
-    canvas.setAttribute("height", img4.height);
+    canvas.setAttribute("width", img1.width);
+    canvas.setAttribute("height", img1.height);
     var cx = canvas.getContext("2d");
     
-    flipHorizontally(cx, img4.width / 2);
-    cx.drawImage(img4, 
-            0, 0, img4.width, img4.height);
+    flipHorizontally(cx, img1.width / 2);
+    cx.drawImage(img1, 
+            0, 0, img1.width, img1.height);
     
     var div1 = document.getElementById("div1");
     //div1.appendChild(elt("p", "", "Flipped canvas:"));
@@ -44,17 +44,17 @@ img4.addEventListener("load", function() {
     
     // Save the temporary canvas as an img.
     
-    canvas34_img = document.createElement("img");
-    canvas34_img.src = canvas.toDataURL();
+    canvas_img = document.createElement("img");
+    canvas_img.src = canvas.toDataURL();
     
     // Draw the flipped img onto the exercise canvas.
     // --Q: Do I need another load event listener for this img tag? Without it, 
     // I seem to remember at least one time that the image did not seem to completely
     // load, but now I cannot reproduce the problem.
     
-    canvas34_img.addEventListener("load", function() {
-        expect("load canvas34_img", "load canvas34_img");
-        cx1.drawImage(canvas34_img, 0, 0, canvas1.width, canvas1.height);
+    canvas_img.addEventListener("load", function() {
+        expect("load canvas_img", "load canvas_img");
+        cx1.drawImage(canvas_img, 0, 0, canvas1.width, canvas1.height);
     });
 });
 
