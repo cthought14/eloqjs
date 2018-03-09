6d85a17ee08d5ca455ac0b5c115c5445dd0992ee
cthought14 <chaotic_thought@yahoo.com>
2018-03-09 07:59:36 +0200

    Continue Chapter 17 notes.

--- ch17/ch17_notes.js
+++ ch17/ch17_notes.js
@@ -8,6 +8,31 @@ $(document).ready(function() {
 //
 tprint("### 336 The protocol");
 
+//
+// 338
+//
+tprint("### 338 Browsers and HTTP");
+expect(encodeURIComponent("Hello & goodbye"), "Hello%20%26%20goodbye");
+expect(decodeURIComponent("Hello%20%26%20goodbye"), "Hello & goodbye");
+
+//
+// 340
+//
+tprint("### 340 XMLHttpRequest");
+
+//
+// 340
+//
+tprint("### 340 Sending a request");
+$('#get1').click(function(ev) {
+    ev.preventDefault();
+    var req = new XMLHttpRequest();
+    req.open("GET", "example/data.txt", false);
+    req.send(null);
+    console.log(req.responseText);
+});
+
+   
 
 
 }); /* $(document).ready */
