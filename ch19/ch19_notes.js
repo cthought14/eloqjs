ef2403f5deed9abeced69c57f6ad16a7fa875945
cthought14 <chaotic_thought@yahoo.com>
2018-03-25 10:30:53 +0200

    Continue Chapter 19 notes.

--- ch19/ch19_notes.js
+++ ch19/ch19_notes.js
@@ -29,6 +29,72 @@ function elt3(name, attributes) {
     return node;
 }
 
+tprint("### 373 The foundation");
+
+var controls = Object.create(null);
+
+function createPaint(parent) {
+    var canvas = elt3("canvas", {width: 500, height: 300});
+    var cx = canvas.getContext("2d");
+    var toolbar = elt3("div", {class: "toolbar"});
+    for (var name in controls)
+        toolbar.appendChild(controls[name](cx));
+    
+    var panel = elt3("div", {class: "picturepanel"}, canvas);
+    parent.appendChild(elt3("div", null, panel, toolbar));
+}
+
+tprint("### 374 Tool selection");
+
+var tools = Object.create(null);
+
+var MOUSE_LEFT_BUTTON = 1;
+
+controls.tool = function(cx) {
+    var select = elt3("select");
+    for (var name in tools)
+        select.appendChild(elt3("option", null, name));
+        
+    cx.canvas.addEventListener("mousedown", function(ev) {
+        if (ev.which == MOUSE_LEFT_BUTTON) {
+            tools[select.value](ev, cx);
+            ev.preventDefault();
+        }
+    });
+    
+    return elt3("span", null, "Tool: ", select);
+};
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
 
 }); /* $(document).ready */
 
