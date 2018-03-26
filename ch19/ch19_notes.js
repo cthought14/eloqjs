271dcd723b2c8f3dde6a28611958fbd33b3fbd51
cthought14 <chaotic_thought@yahoo.com>
2018-03-26 20:03:26 +0200

    Continue Chapter 19 Notes.

--- ch19/ch19_notes.js
+++ ch19/ch19_notes.js
@@ -164,6 +164,259 @@ controls.save = function(cx) {
 //
 tprint("### 380 Loading image files");
 
+function loadImageURL(cx, url) {
+    var image = document.createElement("img");
+    image.addEventListener("load", function() {
+        var color = cx.fillStyle;
+        var size = cx.lineWidth;
+        cx.canvas.width = image.width;
+        cx.canvas.height = image.height;
+        cx.drawImage(image, 0, 0);
+        cx.fillStyle = color;
+        cx.strokeStyle = color;
+        cx.lineWidth = size;
+    });
+    image.src = url;
+}
+
+controls.openFile = function(cx) {
+    var input = elt3("input", {type: "file"});
+    input.addEventListener("change", function() {
+        if (input.files.length == 0)
+            return;
+        var reader = new FileReader();
+        reader.addEventListener("load", function() {
+            loadImageURL(cx, reader.result);
+        });
+        reader.readAsDataURL(input.files[0]);
+    });
+    return elt3("div", null, "Open file: ", input);
+};
+
+controls.openURL = function(cx) {
+    var input = elt3("input", {type: "text"});
+    var form = elt3("form", null,
+                    "Open URL: ", input, 
+                    elt("button", {type: "submit"}, "load"));
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        loadImageURL(cx, input.value);
+    });
+    return form;
+};
+
+//
+// 382
+//
+tprint("### 382 Finishing up");
+
+tools.Text = function(ev, cx) {
+    var text = prompt("Text:", "");
+    if (text) {
+        var pos = relativePos(ev, cx.canvas);
+        cx.font = clamp(cx.lineWidth, 7, 100) + "px sans-serif";
+        cx.fillText(text, pos.x, pos.y);
+    }
+};
+
+tools.Spray = function(ev, cx) {
+    var radius  = cx.lineWidth / 2;
+    var area = radius * radius * Math.PI;
+    var dotsPerTick = Math.ceil(area / 30);
+    
+    var currentPos = relativePos(ev, cx.canvas);
+    var spray = setInterval(function() {
+        for (var i = 0; i < dotsPerTick; i++) {
+            var offset = randomPointInRadius(radius);
+            cx.fillRect(currentPos.x + offset.x,
+                        currentPos.y + offset.y, 1, 1);
+        }
+    }, 25);
+    trackDrag(function(ev) {
+        currentPos = relativePos(ev, cx.canvas);
+    }, function() {
+        clearInterval(spray);
+    });
+};
+
+function randomPointInRadius(radius) {
+    for (;;) {
+        var x = Math.random() * 2 - 1;
+        var y = Math.random() * 2 - 1;
+        if (x * x + y * y <= 1) // => Then (x,y) lies in the unit circle.
+            return {x: x * radius, y: y * radius};
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
