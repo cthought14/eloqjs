90d92a2d6edda35479ca4d48ece5681a2641a357
cthought14 <chaotic_thought@yahoo.com>
2018-04-07 10:33:06 +0200

    Review Chapters 1 and 2.

---
+++ ch2/review/are_these_keywords.js
@@ -0,0 +1,33 @@
+// Chapter 2.
+
+/* 	--Q: Are the following truly keywords?
+		implements interface let package private
+		protected public static yield
+*/
+
+var implements = 1;
+console.log(implements);
+
+var interface = 1;
+console.log(interface);
+
+var let = 1;
+console.log(let);
+
+var package = 1;
+console.log(package);
+
+var private = 1;
+console.log(private);
+
+var protected = 1;
+console.log(protected);
+
+var public = 1;
+console.log(public);
+
+var static = 1;
+console.log(static);
+
+var yield = 1;
+console.log(yield);
